8bd96eb8db030957b52e5d3593b13c82f0505a1e
Udayyv-ux <udayvardhan15@gmail.com>
2025-01-12 17:27:31 +0530

    Add files via upload

---
+++ day2 (2).py
@@ -0,0 +1,16 @@
+ 
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])  
+print(my_list[2])   
+print(my_list[-1]) 
+
+ 
+my_tuple = (100, 200, 300, 400, 500)
+print(my_tuple[1])   
+print(my_tuple[3])  
+print(my_tuple[-2])  
+
+ 
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+print(my_dict['b'])   
+print(my_dict['d'])   
