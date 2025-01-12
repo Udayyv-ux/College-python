64cd15217bedde15beb95cc6782c37e1653ef5bb
Udayyv-ux <udayvardhan15@gmail.com>
2025-01-12 17:46:53 +0530

    Add files via upload

---
+++ day3 (2).py
@@ -0,0 +1,14 @@
+marks1 = float(input("Enter marks for subject 1: "))
+marks2 = float(input("Enter marks for subject 2: "))
+marks3 = float(input("Enter marks for subject 3: "))
+
+average = (marks1 + marks2 + marks3) / 3
+
+if average >= 90:
+    print("Grade: A")
+elif 80 <= average < 90:
+    print("Grade: B")
+elif 70 <= average < 80:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
